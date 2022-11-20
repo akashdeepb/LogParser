f7851bac93ad3d7205ef7ac910c6aa20af8486c0
Akashdeep Bhagat <akashdeepb.cs17@sahyadri.edu.in>
2022-11-20 23:09:05 +0530

    Update LogParser.ts

--- API/src/LogParser.ts
+++ API/src/LogParser.ts
@@ -1,10 +1,15 @@
 import fs from 'fs';
 
+/**
+* LogParser Class
+* Handles Log File functions
+**/
 export class LogParser {
     Constructor () {
         // Default Constructor
     }
 
+    // Function to get data from passed logfile (filepath)
     getDataFromLogFile(filepath: string) {
         return new Promise((resolve, reject) => {
             fs.readFile(filepath, { encoding: 'utf-8' }, (err, data) => {
@@ -14,14 +19,17 @@ export class LogParser {
         });
     }
 
+    // Function to get time from a single log
     getLogTime(text: string) {
         return text.slice(0,24);
     }
 
+    // Function to get type of log passed
     getLogType(text: string) {
         return text.slice(24).split("-", 2)[1];
     }
 
+    // Function to convert given error log to object
     createErrorLog(
         timestamp: number, 
         loglevel: string, 
@@ -36,20 +44,29 @@ export class LogParser {
         }
     }
 
+    // Function to parse errors from given logfile content
     parseErrors(text: string) {
         let errors: Object[] = [];
         const lines = text.split("\n");
+        
+        // Iterate over lines
         for(let i=0;i<lines.length;i++) {
             const logtype = this.getLogType(lines[i]);
+            
+            // Validate if its an error or warn log
             if(logtype === 'warn' || logtype === 'error') {
                 const logtime = this.getLogTime(lines[i]);
                 const details = JSON.parse(lines[i].slice(logtime.length + logtype.length + 2));
                 const timestamp = new Date(logtime).getTime();
                 const transactionId = details.transactionId;
                 const err = details.err;
+                
+                // Push the error to errors array
                 errors.push(this.createErrorLog(timestamp, logtype, transactionId, err))
             }
         }
+        
+        // Return errors
         return errors;
     }
 
