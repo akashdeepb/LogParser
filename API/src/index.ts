f51d5948ffc27771554039803af82aa556157738
Akashdeep Bhagat <akashdeepb.cs17@sahyadri.edu.in>
2022-11-20 23:05:25 +0530

    Added Documentation

--- API/src/index.ts
+++ API/src/index.ts
@@ -6,18 +6,23 @@ import { LogParser } from './LogParser';
 const app = express();
 const upload = multer({ dest: './uploads/' });
 
-app.use(cors());
+app.use(cors());    // Enable CORS
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;  // Port to host server on
 
+// Route to handle Log file upload
 app.use('/upload', upload.single('logfile'), (req, res) => {
     const filename = req.file?.filename;
     const filepath = path.resolve('./uploads/' + filename);
-    const logParser  = new LogParser();
+    const logParser  = new LogParser();     // Create LogParser instance
+    
+    // Parse Uploaded Log File
     logParser.getDataFromLogFile(filepath)
         .then(data => {
             console.log(data);
             const errors = logParser.parseErrors(data);
+        
+            // Response with Errors Array
             res.status(200).json({errors});
         })
         .catch(err => {
@@ -26,6 +31,7 @@ app.use('/upload', upload.single('logfile'), (req, res) => {
         });
 });
 
+// Start Server
 app.listen(port, () => {
     console.log(`Backend for Beanstalk Assignment running @${port}`);
 });
